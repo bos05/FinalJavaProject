b721c8b28c4a78bfaba0f14402a3ac187d3f4f44
Boston <lingwallboston609@gmail.com>
2023-04-21 14:52:08 -0600

    I added made my own tiles and deleted the others/ I also made a load map method that converts txt files to maps on screen

--- src/game/model/tile/TileManager.java
+++ src/game/model/tile/TileManager.java
@@ -1,7 +1,10 @@
 package game.model.tile;
 
 import java.awt.Graphics2D;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import javax.imageio.ImageIO;
 
@@ -11,14 +14,18 @@ public class TileManager
 {
 	GamePanel gp;
 	Tile[] tile;
+	int mapTileNum [][];
 	
 	public TileManager(GamePanel gp)
 	{
 		this.gp = gp;
 		
 		tile = new Tile[10];//10 types of tiles
+		mapTileNum = new int[gp.maxScreenCol] [gp.maxScreenRow];
 		
+			
 		getTileImage();
+		loadMap("/maps/map_1.txt");
 	}
 	
 	public void getTileImage()
@@ -26,13 +33,16 @@ public class TileManager
 		try
 		{
 			tile[0] = new Tile();
-			tile[0].image = ImageIO.read(getClass().getResourceAsStream("/tiles/grass.png"));
-
-			tile[1] = new Tile();
-			tile[1].image = ImageIO.read(getClass().getResourceAsStream("/tiles/wall.png"));
+			tile[0].image = ImageIO.read(getClass().getResourceAsStream("/tiles/body_grass_1.png"));
 			
+			tile[1] = new Tile();
+			tile[1].image = ImageIO.read(getClass().getResourceAsStream("/tiles/body_grass_2.png"));
+
 			tile[2] = new Tile();
-			tile[2].image = ImageIO.read(getClass().getResourceAsStream("/tiles/water.png"));
+			tile[2].image = ImageIO.read(getClass().getResourceAsStream("/tiles/body_bricks_1.png"));
+			
+			tile[3] = new Tile();
+			tile[3].image = ImageIO.read(getClass().getResourceAsStream("/tiles/body_water_1.png"));
 		}
 		catch(IOException e)
 		{
@@ -40,16 +50,57 @@ public class TileManager
 		}
 	}
 	
+	public void loadMap(String filePath)
+	{
+		try
+		{
+			InputStream is = getClass().getResourceAsStream(filePath);
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+			
+			int col = 0;
+			int row  = 0;
+			
+			while(col < gp.maxScreenCol && row < gp.maxScreenRow)
+			{
+				String line = br.readLine();
+				
+				while(col < gp.maxScreenCol)
+				{
+					String numbers[] = line.split(" ");
+					
+					int num = Integer.parseInt(numbers[col]);
+					
+					mapTileNum[col][row] = num;
+					col++;
+				}
+				if(col == gp.maxScreenCol)
+				{
+					col = 0;
+					row++;
+				}
+			}
+			br.close();
+		}
+		catch(Exception e)
+		{
+			
+		}
+		
+	}
+	
 	public void draw(Graphics2D g2)
 	{
 		int col =  0;
 		int row = 0;
 		int x = 0;
-		int y = 0;
+		int y = 0;		
 		
 		while(col < gp.maxScreenCol && row < gp.maxScreenRow)
 		{
-			g2.drawImage(tile[0].image, x, y, gp.tileSize, gp.tileSize, null);
+			int tileNum = mapTileNum[col][row];
+			//everything from text file is stored in this
+			
+			g2.drawImage(tile[tileNum].image, x, y, gp.tileSize, gp.tileSize, null);
 			col++;
 			x += gp.tileSize;
 			
@@ -61,5 +112,20 @@ public class TileManager
 				y += gp.tileSize;
 			}
 		}
+			/*
+			while(col < gp.maxScreenCol && row < gp.maxScreenRow)
+			{
+				g2.drawImage(tile[3].image, x, y, gp.tileSize, gp.tileSize, null);
+				col++;
+				x += gp.tileSize;
+				
+				if(col == gp.maxScreenCol)
+				{
+					col = 0;
+					x = 0;
+					row++;
+					y += gp.tileSize;
+				}
+		}*///testing out new tiles
 	}
 }
