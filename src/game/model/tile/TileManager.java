47fa729105e32e7052cc40b6be307b39c3762338
Boston <lingwallboston609@gmail.com>
2023-04-23 14:26:15 -0600

    made the game more efficient by only rendering the tiles that are on the screen

--- src/game/model/tile/TileManager.java
+++ src/game/model/tile/TileManager.java
@@ -107,16 +107,22 @@ public class TileManager
 			int screenX = worldX - gp.player.worldX + gp.player.screenX;
 			int screenY = worldY - gp.player.worldY + gp.player.screenY;
 			
+			if(worldX + gp.tileSize > gp.player.worldX - gp.player.screenX &&
+			   worldX - gp.tileSize < gp.player.worldX + gp.player.screenX &&
+		       worldY + gp.tileSize * 2 > gp.player.worldY - gp.player.screenY &&
+			   worldY - gp.tileSize * 2 < gp.player.worldY + gp.player.screenY)
+			{
 			g2.drawImage(tile[tileNum].image, screenX, screenY, gp.tileSize, gp.tileSize, null);
-			worldCol++;
-
+			}
 			
+			worldCol++;
 			if(worldCol == gp.maxWorldCol)
 			{
 				worldCol = 0;
 				worldRow++;
 
 			}
+			
 		}
 			/*
 			while(col < gp.maxScreenCol && row < gp.maxScreenRow)
