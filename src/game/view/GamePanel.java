85e1b0cd4c91652bf9610423108dc59e3b535c07
bos05 <111297813+bos05@users.noreply.github.com>
2023-04-25 09:38:29 -0600

    making progress on my map

--- src/game/view/GamePanel.java
+++ src/game/view/GamePanel.java
@@ -31,7 +31,7 @@ public class GamePanel extends JPanel implements Runnable
 
 	//WORLD SETTINGS
 	public final int maxWorldCol = 100;
-	public final int maxWorldRow = 50;
+	public final int maxWorldRow = 75;
 	public final int worldWidth = tileSize * maxWorldCol;
 	public final int worldHeight = tileSize * maxWorldRow;
 	
