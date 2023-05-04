e0932d58f552e6cd2ccf79d9d0ca56ddaf67547c
Boston <lingwallboston609@gmail.com>
2023-05-04 17:50:26 -0600

    added documentation for my eventHandler class

--- src/game/model/EventHandler.java
+++ src/game/model/EventHandler.java
@@ -3,13 +3,22 @@ package game.model;
 import java.awt.Rectangle;
 
 import game.view.GamePanel;
-
+/**
+ * EventHandler creates and looks for 
+ * events such as taking damage and healing
+ * @author Boss_
+ *
+ */
 public class EventHandler 
 {
 	GamePanel gp;
 	Rectangle eventRect;
 	int eventRectDefaultX, eventRectDefaultY;
-	
+	/**
+	 * This is the constructor and it also has some things
+	 *  like reventRect that will be used throughout the code
+	 * @param gp
+	 */
 	public EventHandler(GamePanel gp)
 	{
 		this.gp = gp;
@@ -23,7 +32,9 @@ public class EventHandler
 		eventRectDefaultY = eventRect.y;
 		
 	}
-	
+	/**
+	 * Looks for event then does according thing 
+	 */
 	public void checkEvent()
 	{
 		if(hit(2, 2, "any") == true)
@@ -31,6 +42,13 @@ public class EventHandler
 			gp.player.life -= 1;
 		}
 	}
+	/**
+	 * checks for interaction of something to see if there is collision/interacts with zone
+	 * @param eventCol
+	 * @param eventRow
+	 * @param reqDirection
+	 * @return
+	 */
 	public boolean hit(int eventCol, int eventRow, String reqDirection)
 	{
 		boolean hit = false;
